64aa514463c0e55ab8900810c16a868eac8964b5
EternalEffy <39722892+EternalEffy@users.noreply.github.com>
2021-02-10 22:26:16 +0300

    Переработаны методы readFromFile,countOfSymbol,countOfWords,countOfProposal

--- src/Files.java
+++ src/Files.java
@@ -4,14 +4,12 @@ import java.util.Scanner;
 public class Files {
 
     public static void main(String arg[]) throws IOException {
-        constructFile myFile = new constructFile();
+        ConstructFile myFile = new ConstructFile();
         System.out.println("Введите имя файла");
         Scanner console = new Scanner(System.in);
-        myFile.fileName = console.nextLine();
-        File currentFile = new File(myFile.fileName);
-        myFile.byteArray = new byte[(int)currentFile.length()];
-        myFile.readFromFile();
+        String fileName = console.nextLine();
 
+        myFile.readFromFile(fileName);
         System.out.println(myFile.countOfSymbol());
         System.out.println(myFile.countOfWords());
         System.out.println(myFile.countOfProposal());
--- src/constructFile.java
+++ src/constructFile.java
@@ -1,35 +1,49 @@
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
-public class constructFile {
-    public static String fileName;
-    public static byte[] byteArray;
+class ConstructFile {
+    //private String fileName;
+    private static byte[] byteArray;
 
-    public void readFromFile() throws IOException {
-        File currentFile = new File(fileName);
-        FileInputStream inputReader = new FileInputStream(currentFile);
-        byte[] byteArray = new byte[(int)currentFile.length()];
-        inputReader.read(byteArray);
-        this.byteArray = byteArray;
-    }
-    public static String countOfSymbol(){
-        return (String.valueOf(byteArray.length));
+   /* public ConstructFile(String fileName){
+        this.fileName = fileName;
+    } */
+
+   public void readFromFile(String str) {
+       File currentFile = new File(str);
+       FileInputStream inputReader = null;
+       try {
+           inputReader = new FileInputStream(currentFile);
+       } catch (FileNotFoundException e) {
+           e.printStackTrace();
+       }
+       byte[] byteArray = new byte[(int) currentFile.length()];
+       try {
+       inputReader.read(byteArray);
+       } catch (IOException e) {
+           e.printStackTrace();
+       }
+       this.byteArray = byteArray;
+   }
+
+
+    public int countOfSymbol(){
+        return new String(byteArray).length();
     }
 
-    public static int countOfWords(){
+    public int countOfWords(){
         int countWords=0;
-        String dataFile = new String(byteArray);
-        String [] data = dataFile.split(" +");
+        String [] data = new String(byteArray).split(" +");
         for (int i=0; i<data.length; i++) {
             countWords++;
         }
         return countWords;
     }
-    public static int countOfProposal(){
+    public int countOfProposal(){
         int countProposal=0;
-        String dataFile = new String(byteArray);
-        char[] charArray = dataFile.toCharArray();
+        char[] charArray = new String(byteArray).toCharArray();
         for(int i=0;i<charArray.length;i++)
             if(charArray[i]=='.' || charArray[i]=='!' || charArray[i]=='?'){
                 countProposal++;
